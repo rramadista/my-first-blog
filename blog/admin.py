f4b938ec320cb8fbf1692d13f88f5da6159eb20e
rramadista <raden.ramadista@gmail.com>
2018-07-29 11:04:07 +0700

    Added views to add comment into the post.

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_date', 'published_date')    
 
 admin.site.register(Post, BlogAdmin)
+admin.site.register(Comment)
--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/accounts/login/', views.login, name='login'),
+    path('blog/accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/blog/'}),
     path('polls/', include('polls.urls')),
     path('blog/', include('blog.urls')),
 ]
